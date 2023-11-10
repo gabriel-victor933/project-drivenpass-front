fd2b668732f0360a254defa185e23e730366b866
gabriel-victor933 <gabrielvictor933@usp.br>
2023-11-10 14:51:42 -0300

    feat: adding pages routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,28 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import GlobalStyle from "./styles/GlobalStyle"
 
 function App() {
 
   return (
     <>
+      <GlobalStyle />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/"  element={<h1>layout tela de login</h1>}> {/*telas de login*/}
+            <Route index element={<h1>Tela de login</h1>}/>
+            <Route path="signup" element={<h1>Tela de signup</h1>} />
+          </Route> 
+          <Route path="/home" element={<h1>Layout Tela HOME</h1>}>{/*telas de home*/}
+            <Route index element={<h1>Minhas senhas</h1>} />
+            <Route path=":type" element={<h1>Listagem de tipo selecionado</h1>} />
+            <Route path=":type/:id" element={<h1>Informação da senha selecionada</h1>} />
+            <Route path="register-type" element={<h1>Escolha do tipo de senha</h1>}/>
+            <Route path="register-datas" element={<h1>Cadastro das infos da senhas</h1>}/>
+          </Route> 
         </Routes>
-
       </BrowserRouter>
     </>
   )
 }
 
 export default App
-
-function Home(){
-  return(
-    <>
-    <h1>home</h1>
-    <Link to="/about">About</Link>
-    </>
-
-  )
-}
-
-
-function About(){
-  return(
-    <>
-    <h1>About</h1>
-    <Link to="/">home</Link>
-    </>
-
-  )
-}
---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    background-color: red;
+`
+
+export default GlobalStyle
+
