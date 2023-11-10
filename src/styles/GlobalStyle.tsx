177fee339015516df0839fd54322cc2acfb5731f
gabriel-victor933 <gabrielvictor933@usp.br>
2023-11-10 14:58:54 -0300

    feat: adding global css style

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -1,7 +1,31 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-    background-color: red;
+    * {
+        box-sizing: border-box;
+    }
+
+    html, body {
+        margin: 0;
+        padding: 0;
+        background-color: #DBDBDB;
+        font-family: "Recursive","Inter", sans-serif;
+    }
+
+    body::-webkit-scrollbar {
+    display: none;
+    }
+
+    body {
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+    }
+
+    #root {
+        min-height: 100vh;
+        display: flex;
+        flex-direction: column;
+    }
 `
 
 export default GlobalStyle
